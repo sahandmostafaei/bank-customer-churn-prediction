8b8bdd3f80d44ed424a3795319afba683f8f1b41
Sahand Mostafaei <sahandmostafaei8@gmail.com>
2026-07-22 19:34:08 +0330

    Implement visualization module for customer churn
    Added functions to create output folder and plot target distribution.

--- visualization.py
+++ visualization.py
@@ -1,1 +1,32 @@
+"""
+Visualization Module
+Author: Sahand Mostafaei
+"""
 
+import os
+import matplotlib.pyplot as plt
+
+
+def create_output_folder():
+    os.makedirs("figures", exist_ok=True)
+
+
+def plot_target_distribution(df):
+
+    create_output_folder()
+
+    plt.figure(figsize=(6,4))
+
+    df["Exited"].value_counts().plot(kind="bar")
+
+    plt.title("Customer Churn Distribution")
+
+    plt.xlabel("Exited")
+
+    plt.ylabel("Count")
+
+    plt.tight_layout()
+
+    plt.savefig("figures/churn_distribution.png")
+
+    plt.close()
