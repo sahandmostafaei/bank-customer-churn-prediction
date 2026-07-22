b9a6d62d2e40a25b8702e12264a6703f82592684
Sahand Mostafaei <sahandmostafaei8@gmail.com>
2026-07-22 19:34:41 +0330

    Add feature importance visualization function

--- explainability.py
+++ explainability.py
@@ -1,1 +1,27 @@
+"""
+Explainability Module
+Author: Sahand Mostafaei
+"""
 
+import matplotlib.pyplot as plt
+
+
+def feature_importance(model, X):
+
+    if hasattr(model, "feature_importances_"):
+
+        importance = model.feature_importances_
+
+        plt.figure(figsize=(10,5))
+
+        plt.bar(X.columns, importance)
+
+        plt.xticks(rotation=45)
+
+        plt.title("Feature Importance")
+
+        plt.tight_layout()
+
+        plt.savefig("figures/feature_importance.png")
+
+        plt.close()
