3f43703559479c385e9aa79b5ccf3a979b88efe4
Sahand Mostafaei <sahandmostafaei8@gmail.com>
2026-07-22 19:32:25 +0330

    Implement data loading and cleaning functions
    Added functions to load, clean, and split data.

--- preprocessing.py
+++ preprocessing.py
@@ -1,1 +1,27 @@
+"""
+Data Preprocessing Module
+Author: Sahand Mostafaei
+"""
 
+import pandas as pd
+
+
+def load_data(path):
+
+    return pd.read_csv(path)
+
+
+def clean_data(df):
+
+    df = df.drop_duplicates()
+
+    return df
+
+
+def split_features_target(df):
+
+    X = df.drop(columns=["Exited"])
+
+    y = df["Exited"]
+
+    return X, y
