9e1113528f278c3853e09957b2305285bf997a3e
Sahand Mostafaei <sahandmostafaei8@gmail.com>
2026-08-29 03:03:09 +0330

    Update visualization.py

--- visualization.py
+++ visualization.py
@@ -4,7 +4,13 @@ Author: Sahand Mostafaei
 """
 
 import os
+
 import matplotlib.pyplot as plt
+
+from sklearn.metrics import (
+    ConfusionMatrixDisplay,
+    RocCurveDisplay,
+)
 
 
 def create_output_folder():
@@ -15,18 +21,84 @@ def plot_target_distribution(df):
 
     create_output_folder()
 
-    plt.figure(figsize=(6,4))
+    plt.figure(figsize=(6, 4))
 
-    df["Exited"].value_counts().plot(kind="bar")
+    df["Exited"].value_counts().sort_index().plot(
+        kind="bar"
+    )
 
     plt.title("Customer Churn Distribution")
-
     plt.xlabel("Exited")
-
     plt.ylabel("Count")
 
     plt.tight_layout()
 
-    plt.savefig("figures/churn_distribution.png")
+    plt.savefig(
+        "figures/churn_distribution.png"
+    )
+
+    plt.close()
+
+
+def plot_confusion_matrix(
+    y_test,
+    predictions,
+    model_name,
+):
+
+    create_output_folder()
+
+    ConfusionMatrixDisplay.from_predictions(
+        y_test,
+        predictions
+    )
+
+    plt.title(
+        f"Confusion Matrix - {model_name}"
+    )
+
+    plt.tight_layout()
+
+    filename = (
+        model_name.lower()
+        .replace(" ", "_")
+        + "_confusion_matrix.png"
+    )
+
+    plt.savefig(
+        f"figures/{filename}"
+    )
+
+    plt.close()
+
+
+def plot_roc_curve(
+    y_test,
+    probabilities,
+    model_name,
+):
+
+    create_output_folder()
+
+    RocCurveDisplay.from_predictions(
+        y_test,
+        probabilities
+    )
+
+    plt.title(
+        f"ROC Curve - {model_name}"
+    )
+
+    plt.tight_layout()
+
+    filename = (
+        model_name.lower()
+        .replace(" ", "_")
+        + "_roc_curve.png"
+    )
+
+    plt.savefig(
+        f"figures/{filename}"
+    )
 
     plt.close()
