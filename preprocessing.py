9b5bab9fac8ae6968c2da27e78ade67242c97d55
Sahand Mostafaei <sahandmostafaei8@gmail.com>
2026-08-29 02:59:11 +0330

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -7,21 +7,24 @@ import pandas as pd
 
 
 def load_data(path):
-
     return pd.read_csv(path)
 
 
 def clean_data(df):
-
     df = df.drop_duplicates()
-
     return df
 
 
 def split_features_target(df):
-
     X = df.drop(columns=["Exited"])
-
     y = df["Exited"]
 
-    return X, y
+    categorical_columns = X.select_dtypes(
+        include=["object", "category"]
+    ).columns.tolist()
+
+    numerical_columns = X.select_dtypes(
+        exclude=["object", "category"]
+    ).columns.tolist()
+
+    return X, y, numerical_columns, categorical_columns
